ec35f9b9010fd7e4cd4fd2183d28a05a37c2b153
psyco7042 <81984378+psyco7042@users.noreply.github.com>
2024-09-10 15:07:42 +0530

    template

--- includes/templates/bundle-template.php
+++ includes/templates/bundle-template.php
@@ -16,10 +16,117 @@
         echo '<div class="item-container-main" style="display:flex; gap:20px">';
         $i = 1;
         foreach($product_info as $item_nfo){
-            echo '<div class="item-group" style="padding: 20px; border: 1px solid #ADADAD; border-radius: 15px">';
-            echo "<button id=parent_modal_$i style='outline: none; border: none; background:transparent;'>";
-            echo '<svg height="25pt" viewBox="0 0 480 480" width="25pt" xmlns="http://www.w3.org/2000/svg"><path d="m56 288h136v136c0 26.507812 21.492188 48 48 48s48-21.492188 48-48v-136h136c26.507812 0 48-21.492188 48-48s-21.492188-48-48-48h-136v-136c0-26.507812-21.492188-48-48-48s-48 21.492188-48 48v136h-136c-26.507812 0-48 21.492188-48 48s21.492188 48 48 48zm0 0" fill="#9bc9ff"/><path d="m240 480c-30.914062-.035156-55.964844-25.085938-56-56v-128h-128c-30.929688 0-56-25.070312-56-56s25.070312-56 56-56h128v-128c0-30.929688 25.070312-56 56-56s56 25.070312 56 56v128h128c30.929688 0 56 25.070312 56 56s-25.070312 56-56 56h-128v128c-.035156 30.914062-25.085938 55.964844-56 56zm-184-280c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8zm0 0" fill="#1e81ce"/></svg>';
-            echo '</div>';
+            // echo '<div class="item-group" style="padding: 20px; border: 1px solid #ADADAD; border-radius: 15px">';
+            // echo "<button id=parent_modal_$i style='outline: none; border: none; background:transparent;'>";
+            // echo '<svg height="25pt" viewBox="0 0 480 480" width="25pt" xmlns="http://www.w3.org/2000/svg"><path d="m56 288h136v136c0 26.507812 21.492188 48 48 48s48-21.492188 48-48v-136h136c26.507812 0 48-21.492188 48-48s-21.492188-48-48-48h-136v-136c0-26.507812-21.492188-48-48-48s-48 21.492188-48 48v136h-136c-26.507812 0-48 21.492188-48 48s21.492188 48 48 48zm0 0" fill="#9bc9ff"/><path d="m240 480c-30.914062-.035156-55.964844-25.085938-56-56v-128h-128c-30.929688 0-56-25.070312-56-56s25.070312-56 56-56h128v-128c0-30.929688 25.070312-56 56-56s56 25.070312 56 56v128h128c30.929688 0 56 25.070312 56 56s-25.070312 56-56 56h-128v128c-.035156 30.914062-25.085938 55.964844-56 56zm-184-280c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8zm0 0" fill="#1e81ce"/></svg>';
+            // echo '</div>';
+            ?>
+                <div class="modal-main-container">
+                    <div class="modal hidden">
+                        <div class="products-container">
+                            <?php 
+                                $elements = explode(',', $item_nfo);
+                                foreach ($elements as $element) {
+                                    // Retrieve the product object
+                                    $product = wc_get_product($element);
+                                
+                                    // Make sure the product object is valid
+                                    if ($product) {       
+                                        $currency = get_woocommerce_currency();                                 
+                                        ?>
+                                        <div class="card">
+                                            <div class="card-img">
+                                                <?php
+                                                $image = wp_get_attachment_image_src($product->get_image_id(), 'single-post-thumbnail');
+                                                ?>
+                                                <img src="<?php echo esc_url($image[0]); ?>" alt="<?php echo esc_attr($product->get_name()); ?>">
+                                            </div>
+                                            <div class="card-body">
+                                                <h3 class="card-title"><?php echo esc_html($product->get_name()); ?></h3>
+                                                    <?php
+                                                        if ($product->is_type('variable')){
+                                                            $variations = $product->get_available_variations();
+                                                            if (!empty($variations)) {
+                                                                ?>
+                                                                    <div class="child-modal-container">
+                                                                        <div class="clild-modal hidden">
+                                                                            <div class="variation-container">
+                                                                                <?php
+                                                                                    foreach ($variations as $variation) {
+                                                                                        $variation_id = $variation['variation_id'];
+                                                                                        $variation_product = wc_get_product($variation_id);
+                                                                                        $variation_image_id = $variation_product->get_image_id();
+                                                                                        $variation_image_url = wp_get_attachment_url($variation_image_id);
+                                                                                        $attributes = $variation_product->get_attributes();
+                                                                                        ?>
+                                                                                            <div class="card">
+                                                                                                <div class="card-img">
+                                                                                                    <img src="<?php echo $variation_image_url ?>" alt="<?php echo esc_attr($variation_product->get_name()); ?>">
+                                                                                                </div>
+                                                                                                <div class="card-body">
+                                                                                                    <h3 class="card-title">
+                                                                                                        <?php
+                                                                                                            foreach ($attributes as $taxonomy => $value) {
+                                                                                                                if (strpos($taxonomy, 'pa_') !== false) {
+                                                                                                                    $attribute_name = wc_attribute_label($taxonomy);
+                                                                                                                    $attribute_value = $value;
+                                                                                                                    $formatted_value = ucfirst($attribute_value); 
+                                                                                                                    echo esc_html($product->get_name()) . ' ' . $attribute_value;
+                                                                                                                } else {
+                                                                                                                    $attribute_name = ucfirst($taxonomy); 
+                                                                                                                    $attribute_value = $value; 
+                                                                                                                    
+                                                                                                                    echo esc_html($product->get_name()) . ' ' . $attribute_value;
+                                                                                                                }
+                                                                                                                
+                                                                                                            }
+                                                                                                        ?>
+                                                                                                    </h3>
+                                                                                                    <div class="product-prices">
+                                                                                                        <p class="regular"><?php echo $currency . ' ' . $variation_product->get_regular_price() . '/-';?></p>
+                                                                                                        <p><?php echo $currency . ' ' . $variation_product->get_sale_price() . '/-';?></p>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        <?php
+                                                                                    }
+                                                                                ?>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                <?php
+                                                            }
+                                                        } else {
+                                                            ?>
+                                                                <div class="product-prices">
+                                                                    <p class="regular"><?php echo wc_price($product->get_regular_price()); ?></p>
+                                                                    <p><?php echo wc_price($product->get_sale_price());?></p>
+                                                                </div>
+                                                            <?php
+                                                        }
+                                                    ?>
+                                                
+                                                
+                                            </div>
+                                        </div>
+                                        <?php
+                                    }
+                                }
+                                
+                            ?>
+                        </div>
+                    </div>
+                    <div class="overlay hidden"></div>
+                    <div class="item-group-front">
+                        <button class="btn btn-open">
+                        <svg height="25pt" viewBox="0 0 480 480" width="25pt" xmlns="http://www.w3.org/2000/svg">
+                            <path d="m56 288h136v136c0 26.507812 21.492188 48 48 48s48-21.492188 48-48v-136h136c26.507812 0 48-21.492188 48-48s-21.492188-48-48-48h-136v-136c0-26.507812-21.492188-48-48-48s-48 21.492188-48 48v136h-136c-26.507812 0-48 21.492188-48 48s21.492188 48 48 48zm0 0" fill="#9bc9ff"/>
+                            <path d="m240 480c-30.914062-.035156-55.964844-25.085938-56-56v-128h-128c-30.929688 0-56-25.070312-56-56s25.070312-56 56-56h128v-128c0-30.929688 25.070312-56 56-56s56 25.070312 56 56v128h128c30.929688 0 56 25.070312 56 56s-25.070312 56-56 56h-128v128c-.035156 30.914062-25.085938 55.964844-56 56zm-184-280c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8zm0 0" fill="#1e81ce"/>
+                        </svg>
+                        </button>
+                    </div>
+                </div>
+            <?php
             $i++;
         }
         echo '</div>';
