739939c07fe6716369cb8371f7196c8c66ae8059
psyco7042 <81984378+psyco7042@users.noreply.github.com>
2024-09-18 19:10:30 +0530

    new-feat

--- woocommerce-bundle-commercial.php
+++ woocommerce-bundle-commercial.php
@@ -36,8 +36,8 @@
  add_action('wp_head', 'wbcplugin_ajaxurl');
 
 wp_enqueue_style('virtual-select', plugins_url('assets/css/virtual-select.min.css', __FILE__), '', filemtime(plugin_dir_path(__FILE__) . 'assets/css/virtual-select.min.css'), 'all');
-wp_enqueue_style('main', plugins_url('assets/css/main.css', __FILE__), array('virtual-select'), filemtime(plugin_dir_path(__FILE__) . 'assets/css/main.css'), 'all');
-wp_enqueue_script('main', plugins_url('assets/js/main.js'), array('jquery', 'virtual-select'), filemtime(plugin_dir_path(__FILE__) . 'assets/js/main.js'), true);
+wp_enqueue_style('wbc-main', plugins_url('assets/css/wbc-main.css', __FILE__), array('virtual-select'), filemtime(plugin_dir_path(__FILE__) . 'assets/css/wbc-main.css'), 'all');
+wp_enqueue_script('wbc-main', plugins_url('assets/js/wbc-main.js'), array('jquery', 'virtual-select'), filemtime(plugin_dir_path(__FILE__) . 'assets/js/wbc-main.js'), true);
 wp_enqueue_script('virtual-select', plugins_url('assets/js/virtual-select.min.js', __FILE__), array('jquery'), filemtime(plugin_dir_path(__FILE__) . 'assets/js/virtual-select.min.js'), true);
 wp_enqueue_script('repeater', plugins_url('assets/js/repeater.js', __FILE__), array('jquery'), filemtime(plugin_dir_path(__FILE__) . 'assets/js/repeater.js'), true);
 wp_enqueue_script('parent-modal', plugins_url('assets/js/parent_modal.js', __FILE__), array('jquery'), filemtime(plugin_dir_path(__FILE__) . 'assets/js/parent_modal.js'), true);
